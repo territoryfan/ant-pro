8058b0fcebcec6bed25b71f6b69dfebf512ab13e
territoryfan <33823490+territoryfan@users.noreply.github.com>
2022-03-02 17:23:44 +0800

    Initial commit

---
+++ config/defaultSettings.ts
@@ -0,0 +1,20 @@
+import { Settings as LayoutSettings } from '@ant-design/pro-layout';
+
+const Settings: LayoutSettings & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  navTheme: 'light',
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: true,
+  fixSiderbar: true,
+  title: 'Ant Design Pro',
+  pwa: false,
+  headerHeight: 48,
+  splitMenus: true,
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+};
+
+export default Settings;
